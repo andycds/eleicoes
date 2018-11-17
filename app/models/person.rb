b33c9e23358210a8f79d150aaf3bcf1be9163bd6
Anderson Sanches <andycds@gamail.com>
2018-11-17 18:20:48 -0200

    candidatos conre4

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,17 +11,38 @@ class Person < ActiveRecord::Base
 	end
 
 	def body
-		"<html><body><p>" +
-		"Prezado(a)</p>" +
-		"<p>Estamos encaminhando a seguir o seu login e a sua senha para que você possa exercer o seu direito de votação:</p>" +
-		"<p></p>" +
-		"<p><b>LOGIN: " + self.documento.to_s + "</b></p>" +
-		"<p><b>SENHA: " + self.senha.to_s + "</b></p>" +
-		"<p><a href='https://eleicoes.herokuapp.com/'>https://eleicoes.herokuapp.com/</a></p>" +
-		"<p></p>" +
-		"<p>Início: 21/11 às 0h, encerramento: 22/11 às 20h.</p>" +
-		"<p><b>Obs: login/senha são informações individuais de inteira, única e exclusiva responsabilidade do eleitor. Em caso de perda do login/senha entre em contato com o CONRE</b></p>" +
-		"<p>Exerça o seu direito e vote pela internet. Sua participação no processo eleitoral é muito importante.</p>".to_s
+		if self.conselho == "CONRE2"
+			"<html><body><p>" +
+			"Prezado(a)</p>" +
+			"<p>Estamos encaminhando a seguir o seu login e a sua senha para que você possa exercer o seu direito de votação:</p>" +
+			"<p></p>" +
+			"<p><b>LOGIN: " + self.documento.to_s + "</b></p>" +
+			"<p><b>SENHA: " + self.senha.to_s + "</b></p>" +
+			"<p><a href='https://eleicoes.herokuapp.com/'>https://eleicoes.herokuapp.com/</a></p>" +
+			"<p></p>" +
+			"<p>Início: 21/11 às 0h, encerramento: 22/11 às 20h.</p>" +
+			"<p><b>Obs: login/senha são informações individuais de inteira, única e exclusiva responsabilidade do eleitor. Em caso de perda do login/senha entre em contato com o CONRE</b></p>" +
+			"<p>Exerça o seu direito e vote pela internet. Sua participação no processo eleitoral é muito importante.</p>".to_s
+		else
+			"<html><body><p>" +
+			"Prezado(a) estatístico(a) <b>" + self.nome + "</b>,</p>" +
+			"<p>Conforme Instrução 124 do Conselho Federal de Estatística (CONFE), informamos que a eleição para renovação de 1/3 " +
+			"dos conselheiros do CONRE 4 será das 0h do dia 21/11 até as 20h do dia 22/11. A fim de facilitar a participação, o CONRE 4 " + 
+			"fará a eleição de 2018 <b>exclusivamente pela internet</b>. Você pode ler mais sobre o processo eleitoral, incluindo os candidatos, " + 
+			"neste link: <a href='http://conre4.org.br/Eleic2018'>http://conre4.org.br/Eleic2018</a>.</p>" +
+			"<p>Abaixo estão os dados para que você possa acessar o sistema e realizar o seu voto.</p>" +
+			"<p></p>" +
+			"<p><a href='https://eleicoes.herokuapp.com/'>https://eleicoes.herokuapp.com/</a></p>" +
+			"<p><b>LOGIN: " + self.documento.to_s + "</b></p>" +
+			"<p><b>SENHA: " + self.senha.to_s + "</b></p>" +
+			"<p></p>" +
+			"<p>Participe das eleição e ajude a fazermos uma profissão forte e reconhecida.</p>" +
+			"<p><b>ATENÇÃO</b>: Por determinação do CONFE, a participação na eleição <b>É OBRIGATÓRIA</b>, e o estatístico que não votar nem " +
+			"justificar a sua ausência terá que pagar multa de 50% do valor da anuidade.</p>" +
+			"<p>Atenciosamente, </p>" +
+			"<p>Gabriel Afonso Marchesi Lopes<br/>" +
+			"Presidente CONRE 4 </p>".to_s
+		end
     end
 
     def subject
--- db/conre4.rb
+++ db/conre4.rb
@@ -3,10 +3,11 @@ e5 = Election.where('nome LIKE ?', "%CONRE-4%").first
 
 # Candidatos CONRE-4
 
-c51 = Candidate.create(nome: "Gabriel Afonso Marchesi Lopes".titleize, election: e5)
-c52 = Candidate.create(nome: "Marilene Dias Bandeira".titleize, election: e5)
+c51 = Candidate.create(nome: "Felipe Salerno".titleize, election: e5)
+c52 = Candidate.create(nome: "Marcio Nicolau".titleize, election: e5)
 
 # Eleitores CONRE-4
+
 
 pass = "$2a$10$VUV4fDA3cA.MqOMQsus1BejyOUJRR1VpjwhzaY79EBW5ehO74wu0."
 
